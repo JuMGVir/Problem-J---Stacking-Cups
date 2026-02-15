c3f4ba46ae1399b510efe234fb738bcb24b92e5d
JuMGVir <juanmgviracacha@gmail.com>
2026-02-15 17:38:18 -0500

    Version Base

---
+++ Ciclo 1/Problem J/Tower.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Tower here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+public class Tower{
+    private int width;
+    private int maxHeight;
+    private boolean isOkay;
+    
+    public Tower(int width, int maxHeight){}
+    public void pushCup(int i){}
+    public void popCup(){}
+    public void removeCup(int i){}
+    public void pushLid(int i){}
+    public void popLid(){}
+    public void removeLid(int i){}
+    public void orderTower(){}
+    public void reverseTower(){}
+    public int height(){return 0;}
+    public int[] lidedCups(){return 0[0];}
+    public String[][] stackingItems(){}
+    public void makeVisible(){}
+    public void makeInvisible(){}
+    public void exit(){}
+    public boolean ok(){}
+}
